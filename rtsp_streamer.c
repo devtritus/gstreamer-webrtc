e4358a4315939206baffb2f009f82d2940e52cc3
Ivan <i.lutsenko@outlook.com>
2019-05-29 16:13:00 +0300

    working streamer

--- rtsp_streamer.c
+++ rtsp_streamer.c
@@ -3,9 +3,9 @@
 static void on_pad_added (GstElement *element, GstPad *pad, gpointer data) {
   GstPad *sinkpad;
   GstPadLinkReturn ret;
-  GstElement *depay = (GstElement *) data;
-  g_print ("Dynamic pad created, linking...");
-  sinkpad = gst_element_get_static_pad (depay, "sink");
+  GstElement *sinkelement = (GstElement *) data;
+  g_print ("Dynamic pad created, linking...\n");
+  sinkpad = gst_element_get_static_pad (sinkelement, "sink");
 
   if (gst_pad_is_linked (sinkpad)) {
     g_print ("*** We are already linked ***\n");
@@ -15,6 +15,7 @@ static void on_pad_added (GstElement *element, GstPad *pad, gpointer data) {
     g_print ("processing to linking...\n");
   }
   ret = gst_pad_link (pad, sinkpad);
+  g_print ("%s -> %s\n", gst_pad_get_name(pad), gst_pad_get_name(sinkpad));
 
   if(GST_PAD_LINK_FAILED (ret)) {
     g_print ("failed to link dynamically\n");
@@ -29,7 +30,7 @@ int main (int argc, char *argv[])
 {
   GMainLoop *loop;
 
-  GstElement *pipeline, *rtspsrc, *rtph264depay, *h264parse, *avdec, *xvimagesink;
+  GstElement *pipeline, *rtspsrc, *rtph264depay, *h264parse, *decodebin, *autovideosink;
   GstBus *bus;
 
   GstPad *sinkpad;
@@ -50,10 +51,10 @@ int main (int argc, char *argv[])
   rtspsrc = gst_element_factory_make ("rtspsrc", "rtspsrc");
   rtph264depay = gst_element_factory_make ("rtph264depay", "rtph264depay");
   h264parse = gst_element_factory_make ("h264parse", "h264parse");
-  avdec = gst_element_factory_make ("avdec_h264", "avdec_h264");
-  xvimagesink = gst_element_factory_make ("xvimagesink", "xvimagesink");
+  decodebin = gst_element_factory_make ("decodebin", "decodebin");
+  autovideosink = gst_element_factory_make ("autovideosink", "autovideosink");
 
-  if (!pipeline || !rtspsrc || !rtph264depay || !h264parse || !avdec || !xvimagesink) {
+  if (!pipeline || !rtspsrc || !rtph264depay || !h264parse || !decodebin || !autovideosink) {
     g_printerr ("One of element is not initialized");
     return -1;
   }
@@ -64,15 +65,20 @@ int main (int argc, char *argv[])
       "user-pw", "Barco1984",
       NULL);
 
-  gst_bin_add_many (GST_BIN (pipeline), rtspsrc, rtph264depay, h264parse, avdec, xvimagesink, NULL);
+  gst_bin_add_many (GST_BIN (pipeline), rtspsrc, rtph264depay, h264parse, decodebin, autovideosink, NULL);
 
-  if (!gst_element_link_many (rtph264depay, h264parse, avdec, xvimagesink, NULL)) {
+  if (!gst_element_link_many (rtph264depay, h264parse, decodebin, NULL)) {
     g_error ("Linked failed");
     return -1;
   }
 
   if (!g_signal_connect (rtspsrc, "pad-added", G_CALLBACK (on_pad_added), rtph264depay)) {
     g_error ("Linked failed #2");
+    return -1;
+  }
+
+  if (!g_signal_connect (decodebin, "pad-added", G_CALLBACK (on_pad_added), autovideosink)) {
+    g_error ("Linked failed #3");
     return -1;
   }
 
