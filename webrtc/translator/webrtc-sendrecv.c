3fe0e72a5faba0d6d16634e8dfc6d615e106a2c1
Ivan <i.lutsenko@outlook.com>
2019-06-13 19:06:09 +0300

    l...

--- webrtc/translator/webrtc-sendrecv.c
+++ webrtc/translator/webrtc-sendrecv.c
@@ -330,7 +330,7 @@ start_pipeline (void)
   GError *error = NULL;
   pipe1 = 
       gst_parse_launch ("webrtcbin bundle-policy=max-bundle name=sendrecv " STUN_SERVER
-      " rtspsrc user-id=admin user-pw=Barco1984 location=rtsp://172.17.13.212:554/cam/realmonitor?channel=1&subtype=0 ! queue ! application/x-rtp, media=video, encoding-name=H264, payload=96 ! sendrecv. ",
+      " rtspsrc user-id=admin user-pw=Barco1984 location=rtsp://172.17.13.212:554/cam/realmonitor?channel=1&subtype=0 ! rtph264depay ! rtph264pay config-interval=-1 sprop-parameter-sets=ddd ! sendrecv. ",
       &error);
 
   if (error) {
