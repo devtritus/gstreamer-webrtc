a9309bec9cd93bbc4b89f49088b44dc215b9339e
devtritus <i.lutsenko@outlook.com>
2019-06-18 17:21:40 +0300

    tee

---
+++ pipelines/tee.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+login=`cat ../../camera_login`
+password=`cat ../../camera_password`
+location=`cat ../../camera_location`
+
+gst-launch-1.0 rtspsrc user-id=$login user-pw=$password location=$location ! tee name=t \
+      t. ! queue ! filesink location=../../data/rtp.h264 async=false \
+      t. ! queue ! rtph264depay ! filesink location=../../data/depay.h264 async=false \
+      t. ! queue ! rtph264depay ! h264parse ! filesink location=../../data/depay_parse.h264 async=false \
+      t. ! queue ! rtph264depay ! decodebin ! x264enc ! filesink location=../../data/enc.h264
