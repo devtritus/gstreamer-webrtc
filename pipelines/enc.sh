26f1d7aff5f546dd0b836f02a669c0ce39e933eb
devtritus <i.lutsenko@outlook.com>
2019-06-19 12:53:06 +0300

    ...

--- pipelines/enc.sh
+++ pipelines/enc.sh
@@ -4,4 +4,4 @@ login=`cat ../../camera_login`
 password=`cat ../../camera_password`
 location=`cat ../../camera_location`
 
-gst-launch-1.0 rtspsrc user-id=$login user-pw=$password location=$location ! rtph264depay ! decodebin ! x264enc ! rtph264pay ! rtph264depay ! filesink location=../../xz.h264
+gst-launch-1.0 -v rtspsrc user-id=$login user-pw=$password location=$location ! rtph264depay ! decodebin ! x264enc ! rtph264pay ! filesink location=../../enc.h264
