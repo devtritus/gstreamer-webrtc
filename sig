7757d17113c6c65881633d6e6494031e6c839cbe
devtritus <i.lutsenko@outlook.com>
2019-07-09 18:53:41 +0300

    ...

--- sig
+++ sig
@@ -1,2 +1,2 @@
-cd webrtc_env/signalling
+cd env/signalling
 ./simple-server.py
---
+++ src/video/video_bin.h
@@ -0,0 +1,8 @@
+#ifndef VIDEO_BIN_H_
+#define VIDEO_BIN_H_
+
+#include <gst/gst.h>
+
+GstElement *create_video_bin ();
+
+#endif
