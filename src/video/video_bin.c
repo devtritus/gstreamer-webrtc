a7a0d3ba64270b8fa8c66e28985f949d16ed46fd
devtritus <i.lutsenko@outlook.com>
2019-07-17 19:15:32 +0300

    ...

--- src/video/video_bin.c
+++ src/video/video_bin.c
@@ -1,4 +1,5 @@
 #include <gst/gst.h>
+#include <stdio.h>
 #include <glib.h>
 
 #define STUN_SERVER "stun://stun.l.google.com:19302"
@@ -62,46 +63,34 @@ on_pad_added (GstElement *element,
   gst_object_unref (sinkpad);
 }
 
-static GstElement *
-create_video_bin (char *location, char *login, char *password)
-{
-  GstElement *bin, *rtspsrc, *depay, *tee;
-
-  bin = gst_pipeline_new ("video-bin");
-  rtspsrc   = gst_element_factory_make ("rtspsrc",      "rtspsrc");
-  depay     = gst_element_factory_make ("rtph264depay", "rtph264depay");
-  tee       = gst_element_factory_make ("tee",          "tee");
-
-  if (!bin || !rtspsrc || !depay || !tee) {
-    g_printerr ("One element could not be created. Exiting.\n");
-    return NULL;
-  }
-
-  g_object_set (G_OBJECT (rtspsrc), "location",location,
-                                    "user-id", login,
-                                    "user-pw", password, NULL);
-
-  gst_bin_add_many (GST_BIN (bin), rtspsrc, depay, tee, NULL);
-
-  gst_element_link (depay, tee);
-  g_signal_connect (rtspsrc, "pad-added", G_CALLBACK (on_pad_added), depay);
-
-  return bin;
-}
-
-int main(int argc, char *argv[]) {
+static void *
+create_stream_pipeline(gpointer data) {
   GMainLoop *loop;
 
   GstBus *bus;
   guint bus_watch_id;
-  GstElement * pipeline;
-
-  /* Initialisation */
-  gst_init (&argc, &argv);
+  GstElement *pipeline, *rtspsrc, *depay, *tee;
 
   loop = g_main_loop_new (NULL, FALSE);
   
-  pipeline = create_video_bin("rtsp://192.168.1.108:554/cam/realmonitor?channel=1&subtype=0", "admin", "1236987q");
+  pipeline = gst_pipeline_new ("video-pipeline");
+  rtspsrc   = gst_element_factory_make ("rtspsrc",      "rtspsrc");
+  depay     = gst_element_factory_make ("rtph264depay", "rtph264depay");
+  tee       = gst_element_factory_make ("tee",          "tee");
+
+  if (!pipeline || !rtspsrc || !depay || !tee) {
+    g_printerr ("One element could not be created. Exiting.\n");
+    return NULL;
+  }
+
+  g_object_set (G_OBJECT (rtspsrc), "location", "rtsp://admin:Barco1984@172.17.13.216:554/cam/realmonitor?channel=1&subtype=0",
+                                    "user-id", "admin",
+                                    "user-pw", "Barco1984", NULL);
+
+  gst_bin_add_many (GST_BIN (pipeline), rtspsrc, depay, tee, NULL);
+
+  gst_element_link (depay, tee);
+  g_signal_connect (rtspsrc, "pad-added", G_CALLBACK (on_pad_added), depay);
 
   /* we add a message handler */
   bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
@@ -109,14 +98,11 @@ int main(int argc, char *argv[]) {
   gst_object_unref (bus);
 
   /* Set the pipeline to "playing" state*/
-  g_print ("Now playing: %s\n", argv[1]);
   gst_element_set_state (pipeline, GST_STATE_PLAYING);
-
 
   /* Iterate */
   g_print ("Running...\n");
   g_main_loop_run (loop);
-
 
   /* Out of the main loop, clean up nicely */
   g_print ("Returned, stopping playback\n");
@@ -126,6 +112,24 @@ int main(int argc, char *argv[]) {
   gst_object_unref (GST_OBJECT (pipeline));
   g_source_remove (bus_watch_id);
   g_main_loop_unref (loop);
+
+  return pipeline;
+}
+
+int main(int argc, char *argv[])
+{
+  GThread * pipelineThread;
+  gchar * str = NULL;
+  /* Initialisation */
+  gst_init (&argc, &argv);
+
+  do {
+    scanf("%s", str);
+    if(g_strcmp0 ("run", str) == 0) {
+      pipelineThread = g_thread_new("stream", &create_stream_pipeline, NULL);
+      g_print("%p\n", pipelineThread);
+    }
+  } while (g_strcmp0 ("end", str) != 0);
 
   return 0;
 }
